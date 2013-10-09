b6471c86066cbfcc54289b87a0847deed2d752f5
Deepwinter <deepwinter@winterroot.net>
2013-10-08 19:05:05 -0700

    Added NSDate formatting

---
+++ NSDate+Pretty.h
@@ -0,0 +1,18 @@
+//
+//  NSDate+Pretty.h
+//  joinman
+//
+//  Created by Matthew Shultz on 4/14/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Pretty)
+
+- (NSString *) pretty;
+- (NSString *) dayAndTime;
+- (NSString *) formatted;
+- (NSString *) mysqlFormat;
+
+@end
